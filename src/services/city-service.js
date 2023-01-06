5796240380fdd56139df0e7bb860bd362d6e5896
aman <amanrawat.ar321@gmail.com>
2023-01-06 12:09:20 +0530

    completed city service

---
+++ src/services/city-service.js
@@ -0,0 +1,51 @@
+const { CityRepository } = require('../repository/index');
+
+const cityRepository = new CityRepository();
+class CityService {
+    // here is a proble we don't have to creat3e and instance when we only want to acess behavioud
+    // constructor() {
+    //     this.cityRepository = new CityRepository();
+    // }
+
+    async create(data) {
+        try {
+            const city = await cityRepository.createCity(data);
+            return city;
+        } catch (error) {
+            console.log('something went wrong in service layer');
+            throw { error };
+        }
+     };
+
+    async delete(cityId) {
+        try {
+            const resp = await cityRepository.deleteCity(cityId);
+            return resp;
+        } catch (error) {
+            console.log('something went wrong in service layer');
+            throw { error };
+        }
+     };
+
+    async update(cityId,data) {
+        try {
+            const city = await cityRepository.updateCity(cityId,data);
+            return city;
+        } catch (error) {
+            console.log('something went wrong in service layer');
+            throw { error };
+        }
+     };
+
+    async getCity(cityId) { 
+        try {
+            const city = await cityRepository.getCity(cityId);
+            return city;
+        } catch (error) {
+            console.log('something went wrong in service layer');
+            throw { error };
+        }
+    };
+};
+
+module.exports = CityService;
