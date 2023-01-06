7318f9bdf6ec45f4ad5b58ae7b9238a372121578
aman <amanrawat.ar321@gmail.com>
2023-01-06 13:38:24 +0530

    added city routes and implement crud api

--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
+const ApiRoutes = require('./routes/index');
+
 const setupAndStartService = async () => {
 
     const app = express();
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-    
+
+    app.use('/api',ApiRoutes);
+
     app.listen(PORT, () => {
         console.log(`server is running on PORT ${PORT}`);
     })
