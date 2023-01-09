891310e5359d91bb23033612a997b712cde48f23
aman <amanrawat.ar321@gmail.com>
2023-01-09 21:32:46 +0530

    added airplanes model and seed data

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ const { PORT } = require('./config/serverConfig');
 
 const ApiRoutes = require('./routes/index');
 
+const { sequelize } = require('./models');
+const db = require('./models/index');
+
 const setupAndStartService = async () => {
 
     const app = express();
@@ -13,6 +16,29 @@ const setupAndStartService = async () => {
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use('/api',ApiRoutes);
+
+    if(process.env.SYNC_DB) {
+        db.sequelize.sync({ alter:true })
+    }
+
+    // const airport = await db.Airport.create({
+    //     name:'Bangalore International Airport',
+    //     cityId:5
+    // })
+
+    // const city = await db.City.findOne({
+    //     where: {
+    //         id: 4
+    //     }
+    // });
+    // const airport = await db.Airport.findOne({
+    //     where: {
+    //         id: 6
+    //     }
+    // })
+    // const air = await city.addAirports(airport);
+    // console.log(airport);
+
 
     app.listen(PORT, () => {
         console.log(`server is running on PORT ${PORT}`);
